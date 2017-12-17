60106274d8ef8c783796c15f0d6fac63e3854ef9
Scott Duffy <duffyscottc@gmail.com>
2017-12-17 11:42:29 -0500

    added a constructor and some getters and setters to Edge

--- src/element/Edge.java
+++ src/element/Edge.java
@@ -18,6 +18,17 @@ public class Edge extends Element {
     private Vertex endpoint1;
     private Vertex endpoint2;
     
+    /**
+     * An initializer that allows you to define the endpoints immediately
+     * (Note: endpoints are unordered. Numbered so we can tell them apart.)
+     * @param endpoint1
+     * @param endpoint2 
+     */
+    public Edge (Vertex endpoint1, Vertex endpoint2) {
+        this.endpoint1 = endpoint1;
+        this.endpoint2 = endpoint2;
+    }
+    
     @Override
     public void draw(Graphics2D g2) {
         if (stroke == null) {
@@ -33,6 +44,22 @@ public class Edge extends Element {
         int x2 = (int) endpoint2.getCenter().getX();
         int y2 = (int) endpoint2.getCenter().getY();
         g2.drawLine(x1, y1, x2, y2); //draw the line
+    }
+    
+    public Vertex getEndpoint1() {
+        return endpoint1;
+    }
+
+    public void setEndpoint1(Vertex endpoint1) {
+        this.endpoint1 = endpoint1;
+    }
+
+    public Vertex getEndpoint2() {
+        return endpoint2;
+    }
+
+    public void setEndpoint2(Vertex endpoint2) {
+        this.endpoint2 = endpoint2;
     }
 
     @Override
