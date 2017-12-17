ddd3794c97a3ea6e72cbc7d855f0fb153f6f0754
Scott Duffy <duffyscottc@gmail.com>
2017-12-16 21:16:42 -0500

    added Edge class

--- src/views/Canvas.java
+++ src/views/Canvas.java
@@ -5,10 +5,41 @@
  */
 package views;
 
+import element.Graph;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JTextArea;
+
 /**
  *
  * @author Scott
  */
-public class Canvas {
-    
+public class Canvas extends JTextArea {
+
+    private Graph graph = null;
+
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (graph == null) {
+            return;
+        }
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setRenderingHint(
+            RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON
+        );
+        
+        
+
+    }
+
 }
