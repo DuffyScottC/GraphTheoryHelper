c2176dc8c54de30e899d331a89d4e7642424b81d
Scott Duffy <duffyscottc@gmail.com>
2017-12-16 21:05:30 -0500

    added code from class example that fit well (members and their getters/setters)

--- src/element/Element.java
+++ src/element/Element.java
@@ -5,10 +5,71 @@
  */
 package element;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.Point2D;
+import java.io.Serializable;
+
 /**
  *
  * @author Scott
  */
-public class Element {
-    
+public abstract class Element implements Serializable {
+
+    protected float strokeWidth = 1.0f;
+    protected Shape shape;
+    protected Color strokeColor = Color.BLACK;
+    protected Color fillColor = Color.RED;
+    protected double xLoc = 0;
+    protected double yLoc = 0;
+    protected String title = "A";
+
+    // the stroke is created from strokeWidth + BasicStroke
+    // but BasicStroke is not serializable, so the actual
+    // stroke used is created "on the fly"
+    protected transient Stroke stroke = null;
+
+    // the actual drawing is done by the subclass
+    public abstract void draw(Graphics2D g2);
+
+    public abstract Shape getPositionShape();
+
+    public void setLocation(double xLoc, double yLoc) {
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+    }
+
+    public void incLocation(double xInc, double yInc) {
+        this.xLoc += xInc;
+        this.yLoc += yInc;
+    }
+
+    public Point2D.Double getLocation() {
+        return new Point2D.Double(xLoc, yLoc);
+    }
+
+    public final void setStrokeWidth(float strokeWidth) {
+        this.strokeWidth = strokeWidth;
+        stroke = null;
+    }
+
+    public void setStrokeColor(Color color) {
+        this.strokeColor = color;
+    }
+
+    public void setFillColor(Color color) {
+        this.fillColor = color;
+    }
+
+    public final void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
 }
