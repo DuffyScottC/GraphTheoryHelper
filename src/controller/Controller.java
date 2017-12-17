818440fc71afa149e28c21fa8e89be671c754bd7
Scott Duffy <duffyscottc@gmail.com>
2017-12-17 12:45:19 -0500

    add code to controller to load samples

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,6 +6,8 @@
 package controller;
 
 import element.Graph;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ListSelectionModel;
 import views.Canvas;
 import views.GraphFrame;
@@ -16,33 +18,42 @@ import views.GraphFrame;
  */
 public class Controller {
 
-  private final GraphFrame frame = new GraphFrame();
-  private final Canvas canvas = frame.getCanvas();
-  
-  private final Graph graph = new Graph("Simple Graph");
-  
-  public Controller() {
-    frame.setTitle( "Graph Helper" );
-    frame.setLocationRelativeTo(null);
-    frame.setSize(800, 500);
-    
-    canvas.setGraph(graph); //pass the graph to the canvas
-    
-    //Set up list models:
-    //set them to their respective JLists
-    frame.getVerticesList().setModel(graph.getVerticesListModel());
-    frame.getEdgesList().setModel(graph.getEdgesListModel());
-    //set their selection modes
-    frame.getVerticesList().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-    frame.getEdgesList().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-    
-    //Action listeners:
-    
-    
-  }
+    private final GraphFrame frame = new GraphFrame();
+    private final Canvas canvas = frame.getCanvas();
 
-  public static void main(String[] args) {
-    Controller app = new Controller();
-    app.frame.setVisible(true);
-  }
+    private final Graph graph = new Graph("Simple Graph");
+
+    //Used for moving objects. Holds the last point the mouse was at.
+    private int lastX;
+    private int lastY;
+
+    public Controller() {
+        frame.setTitle("Graph Helper");
+        frame.setLocationRelativeTo(null);
+        frame.setSize(800, 500);
+
+        canvas.setGraph(graph); //pass the graph to the canvas
+
+        //Set up list models:
+        //set them to their respective JLists
+        frame.getVerticesList().setModel(graph.getVerticesListModel());
+        frame.getEdgesList().setModel(graph.getEdgesListModel());
+        //set their selection modes
+        frame.getVerticesList().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        frame.getEdgesList().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        //Action listeners:
+        frame.getLoadSamplesMenuItem().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Helpers.setSampleElements(graph);
+                canvas.repaint();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Controller app = new Controller();
+        app.frame.setVisible(true);
+    }
 }
