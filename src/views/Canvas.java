dd1504c6de4fc18930e3b6652f78ec3e79284083
Scott Duffy <duffyscottc@gmail.com>
2017-12-17 12:49:59 -0500

    draws edges first, then vertices

--- src/views/Canvas.java
+++ src/views/Canvas.java
@@ -38,8 +38,8 @@ public class Canvas extends JTextArea {
             RenderingHints.VALUE_ANTIALIAS_ON
         );
         
-        graph.drawVertices(g2);
         graph.drawEdges(g2);
+        graph.drawVertices(g2);
 
     }
 
