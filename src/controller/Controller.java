13a558e5858db9e6a1c1e2d7ad1e897d25d7144f
Scott Duffy <duffyscottc@gmail.com>
2017-12-20 01:36:43 -0500

    moved save code to its own function

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -33,15 +33,16 @@ public class Controller {
     private final Canvas canvas = frame.getCanvas();
 
     private final Graph graph = new Graph("Simple Graph");
-    
+
     //File I/O:
-    JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+    private JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+    private File saveFile;
 
     public Controller() {
         frame.setTitle("Graph Helper");
         frame.setLocationRelativeTo(null);
         frame.setSize(800, 500);
-        
+
         canvas.setGraph(graph); //pass the graph to the canvas
         graph.setCanvas(canvas); //pass the canvas to the graph
 
@@ -52,7 +53,7 @@ public class Controller {
         //set their selection modes
         frame.getVerticesList().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         frame.getEdgesList().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        
+
         //Action listeners:
         frame.getLoadSamplesMenuItem().addActionListener(new ActionListener() {
             @Override
@@ -61,7 +62,7 @@ public class Controller {
                 canvas.repaint();
             }
         });
-        
+
         frame.getSaveAsMenuItem().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -69,18 +70,18 @@ public class Controller {
                     JOptionPane.showMessageDialog(frame, "Cannot save an empty graph.");
                     return;
                 }
-                
+
                 chooser.setDialogTitle("Save");
-                
+
                 //Open the save dialogue and let the user choose 
                 //where to save the file:
                 int chooserResult = chooser.showSaveDialog(frame);
                 //if the user successfully saved the file
                 if (chooserResult == JFileChooser.APPROVE_OPTION) {
-                    
+
                     //get the path of the file that the user selected
                     Path path = chooser.getSelectedFile().toPath();
-                    
+
                     //check if the file has an extension already
                     String fileName = path.getFileName().toString(); //the name of the file
                     if (!fileName.matches(".*\\.\\w+")) { //if the file name has NO extension
@@ -88,12 +89,12 @@ public class Controller {
                         String fileNameWithExtension = fileName + ".graph";
                         //use the resolveSibling method to change the old, 
                         //extensionless file name to the new filename created above
-                        path = path.resolveSibling(fileNameWithExtension); 
+                        path = path.resolveSibling(fileNameWithExtension);
                         //e.g. this will replace "curdir/sample2" with "curdir/sample2.graph"
                     }
-                    
-                    File saveFile = path.toFile(); //convert the path object to a file object
-                    
+
+                    saveFile = path.toFile(); //convert the path object to a file object
+
                     //check if the file already exists
                     if (Files.exists(path)) { //if the file already exists
                         //ask the user if they want to continue
@@ -102,30 +103,15 @@ public class Controller {
                             return;
                         }
                     }
-                    
+
                     //Save the graph
-                    try {
-                        //Create an output stream from the file
-                        FileOutputStream ostr = new FileOutputStream(saveFile);
-                        
-                        ObjectOutputStream oostr = new ObjectOutputStream(ostr);
-                        
-                        //Save the graph
-                        oostr.writeObject(graph);
-                        oostr.close(); //must do this to ensure completion
-                    } catch (IOException ex) {
-                        
-                        JOptionPane.showMessageDialog(frame, "Unable to save file.\n" + ex.getMessage(), "Oops!", JOptionPane.ERROR_MESSAGE);
-                        
-                    }
-                    
-                    graph.setIsSaved(true);
-                    
+                    saveGraph();
+
                 }
-                
+
             }
         });
-        
+
         frame.getOpenMenuItem().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -134,31 +120,31 @@ public class Controller {
                         return;
                     }
                 }
-                
+
                 graph.setIsSaved(true);
-                
+
                 chooser.setDialogTitle("Open");
                 int chooserResult = chooser.showOpenDialog(frame);
                 if (chooserResult == JFileChooser.APPROVE_OPTION) {
                     File loadFile = chooser.getSelectedFile();
-                    
+
                     try {
                         //create an input stream from the selected file
-                        FileInputStream  istr = new FileInputStream(loadFile); 
-                        ObjectInputStream oistr = new ObjectInputStream( istr );
-                        
+                        FileInputStream istr = new FileInputStream(loadFile);
+                        ObjectInputStream oistr = new ObjectInputStream(istr);
+
                         //load the object from the serialized file
                         Object theObject = oistr.readObject();
                         oistr.close();
-                        
+
                         //if this object is a graph
                         if (theObject instanceof Graph) {
                             //cast the loaded object to a graph
                             Graph loadedGraph = (Graph) theObject;
-                            
+
                             //replace the old graph with the new one
                             graph.replace(loadedGraph);
-                            
+
                             canvas.repaint();
                         }
                     } catch (IOException ex) {
@@ -166,11 +152,11 @@ public class Controller {
                     } catch (ClassNotFoundException ex) {
                         JOptionPane.showMessageDialog(frame, "File is not a figures file.\n" + ex.getMessage(), "Oops!", JOptionPane.ERROR_MESSAGE);
                     }
-                    
+
                 }
             }
         });
-        
+
         frame.getFileMenu().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -181,12 +167,19 @@ public class Controller {
                 }
             }
         });
-        
+
+        frame.getSaveMenuItem().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
         //Drag to move vertices:
         graph.addEventHandlers(frame);
-        
+
     }
-    
+
     /**
      * Convenience method that asks a user if they want to continue if they try
      * to 1) open a file without saving 2) create a new file without saving 3)
@@ -206,6 +199,29 @@ public class Controller {
         //if the file has been saved, then we can just return true
         return true;
 
+    }
+
+    private void saveGraph() {
+        if (saveFile == null) {
+            return;
+        }
+        
+        try {
+            //Create an output stream from the file
+            FileOutputStream ostr = new FileOutputStream(saveFile);
+
+            ObjectOutputStream oostr = new ObjectOutputStream(ostr);
+
+            //Save the graph
+            oostr.writeObject(graph);
+            oostr.close(); //must do this to ensure completion
+        } catch (IOException ex) {
+
+            JOptionPane.showMessageDialog(frame, "Unable to save file.\n" + ex.getMessage(), "Oops!", JOptionPane.ERROR_MESSAGE);
+
+        }
+
+        graph.setIsSaved(true);
     }
 
     public static void main(String[] args) {
