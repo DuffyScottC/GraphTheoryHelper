fb083cf0d048f53ecb9e4c394ee3e74b6faae4f0
Scott Duffy <duffyscottc@gmail.com>
2017-12-31 19:48:32 -0500

    increased clickable positionShape of vertices

--- src/element/Vertex.java
+++ src/element/Vertex.java
@@ -162,7 +162,8 @@ public class Vertex extends Element {
      */
     @Override
     public Shape getPositionShape() {
-        return new Ellipse2D.Double(xLoc, yLoc, diameter, diameter);
+        double strokeBuffer = Values.VERTEX_STROKE_WIDTH/2;
+        return new Ellipse2D.Double(xLoc, yLoc, diameter + strokeBuffer, diameter + strokeBuffer);
     }
 
 }
