5638e5abb735a998cbf2396e536558089c2efe6c
Scott Duffy <duffyscottc@gmail.com>
2018-01-15 11:30:33 -0500

    allowed for completely vertical vectors and created add() method and added documentation

--- src/controller/Vector2D.java
+++ src/controller/Vector2D.java
@@ -5,9 +5,15 @@
  */
 package controller;
 
+import java.awt.geom.Point2D;
+
 /**
  * All code in this particular vector class assumes that the vector is from the
- * origin (0,0) to whatever points you insert. This helps you
+ * origin (0,0) to whatever points you insert. This helps find the angle of the 
+ * vector.<>
+ * Important: Point2D and Vector2D are not interchangeable! Vector2D is not a
+ * position. It conveys only a magnitude and a direction from the origin. To
+ * get a position use the {@link #add(Point2D.Double)} method.
  *
  * @author Scott
  */
@@ -16,11 +22,31 @@ public class Vector2D {
     public double x;
     public double y;
     private int quadrant = 1;
-
+    
+    /**
+     * Creates a vector from the origin (0,0) to the given point (x,y).
+     * @param x The x-value of this vector from the origin
+     * @param y The y-value of this vector from the origin
+     */
     public Vector2D(double x, double y) {
         this.x = x;
         this.y = y;
-
+        findQuadrant();
+    }
+    
+    /**
+     * Creates a vector from the origin (0,0) that has the same magnitude and
+     * direction as the vector from points a to b.
+     * @param a
+     * @param b 
+     */
+    public Vector2D(Point2D.Double a, Point2D.Double b) {
+        this.x = b.x - a.x;
+        this.y = b.y - a.y;
+        findQuadrant();
+    }
+    
+    private void findQuadrant() {
         //determine what quadrant we are in
         if (y > 0) { //y is positive
             if (x > 0) { //x is positive
@@ -45,6 +71,14 @@ public class Vector2D {
      * @return An angle from -pi/2 to 3pi/2 not inclusive (in radians)
      */
     public double getAngle() {
+        if (x == 0) { //if the vector is completely vertical
+            if (y > 0) { //if y is positive
+                return Math.PI/4; //90 degrees, or pi/4 radians
+            } else { //if y is negative
+                return 3*Math.PI/2; //270 degrees, or 3pi/2 radians
+            }
+        } 
+        //if the vector is not completely vertical (x != 0)
         //find the result of arctan(y/x)
         double angle = Math.atan2(y, x);
         //add 180 degrees (pi radians) to angle if this vector is in Q2 or Q3
@@ -58,5 +92,17 @@ public class Vector2D {
             default: //quadrant 4
                 return angle;
         }
+    }
+    
+    /**
+     * Add this vector to the given point.
+     * @param point The point to add this vector to
+     * @return A Point2D.Double object that is this vector's magnitude and
+     * direction from the passed in point.
+     */
+    public Point2D.Double add(Point2D.Double point) {
+        double xPos = point.x + this.x;
+        double yPos = point.y + this.y;
+        return new Point2D.Double(xPos, yPos);
     }
 }
