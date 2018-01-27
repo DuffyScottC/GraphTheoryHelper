2e770148988fd33c76cc832844380aa93b7c6246
Scott Duffy <duffyscottc@gmail.com>
2018-01-27 00:03:08 -0500

    ran into errors when trying to convert to maven

--- src/views/GraphColorChooserDialog.java
+++ src/views/GraphColorChooserDialog.java
@@ -47,8 +47,6 @@ public class GraphColorChooserDialog extends javax.swing.JDialog {
         vertexFillColorChooseButton = new javax.swing.JButton();
         vertexStrokeColorChooseButton = new javax.swing.JButton();
         edgeStrokeColorChooseButton = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        sampleCanvas = new views.SampleCanvas();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -82,10 +80,6 @@ public class GraphColorChooserDialog extends javax.swing.JDialog {
 
         edgeStrokeColorChooseButton.setText("Choose");
 
-        sampleCanvas.setColumns(20);
-        sampleCanvas.setRows(5);
-        jScrollPane1.setViewportView(sampleCanvas);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -93,9 +87,6 @@ public class GraphColorChooserDialog extends javax.swing.JDialog {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1)
-                        .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(cancelButton)
@@ -140,9 +131,7 @@ public class GraphColorChooserDialog extends javax.swing.JDialog {
                     .addComponent(jLabel3)
                     .addComponent(edgeStrokeColorTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(edgeStrokeColorChooseButton))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(okButton)
                     .addComponent(cancelButton))
@@ -205,9 +194,7 @@ public class GraphColorChooserDialog extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton okButton;
-    private views.SampleCanvas sampleCanvas;
     private javax.swing.JButton vertexFillColorChooseButton;
     private javax.swing.JTextField vertexFillColorTextField;
     private javax.swing.JButton vertexStrokeColorChooseButton;
