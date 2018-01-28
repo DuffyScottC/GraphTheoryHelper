8de72f5b9850385a4f0fe3b96b8d93c08bfa6e5f
Scott Duffy <duffyscottc@gmail.com>
2018-01-27 20:03:54 -0500

    after moving images to resource/ set button icons

--- src/main/java/views/GraphFrame.java
+++ src/main/java/views/GraphFrame.java
@@ -222,24 +222,28 @@ public class GraphFrame extends javax.swing.JFrame {
         toolBar.setOrientation(javax.swing.SwingConstants.VERTICAL);
         toolBar.setRollover(true);
 
+        addVerticesButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/add-vertices-Icon.png"))); // NOI18N
         addVerticesButton.setToolTipText("Click to add vertices to the canvas (V)");
         addVerticesButton.setFocusable(false);
         addVerticesButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         addVerticesButton.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         toolBar.add(addVerticesButton);
 
+        addEdgesButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/add-edges-icon.png"))); // NOI18N
         addEdgesButton.setToolTipText("Add/bend edges between vertices (E)");
         addEdgesButton.setFocusable(false);
         addEdgesButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         addEdgesButton.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         toolBar.add(addEdgesButton);
 
+        selectionButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/selection-icon.png"))); // NOI18N
         selectionButton.setToolTipText("Select and move vertices and edges (spacebar)");
         selectionButton.setFocusable(false);
         selectionButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         selectionButton.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         toolBar.add(selectionButton);
 
+        deleteButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/delete-icon.png"))); // NOI18N
         deleteButton.setToolTipText("Delete (backspace)");
         deleteButton.setFocusable(false);
         deleteButton.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
