7c4ff4ec28a91063a8b42482712762ba8653f7dc
Scott Duffy <duffyscottc@gmail.com>
2018-01-31 22:01:37 -0500

    added a button that looks like a hyperlink

---
+++ src/main/java/controller/GraphVersionChecker.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ * Checks to see whether a new update is available for the program on 
+ * GitHub.com when the user opens the program.
+ * @author Scott
+ */
+public class GraphVersionChecker {
+    
+}
